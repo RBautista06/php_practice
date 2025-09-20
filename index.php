4ed4e48aca3570fcf3e817104edba74a13a5bfc9
Railley Nickolei Vince Bautisa <railleynickoleivincebautista@gmail.com>
2025-09-21 03:26:43 +0800

    asd

--- index.php
+++ index.php
@@ -1,6 +1,14 @@
 <?php
+
+require __DIR__.'/vendor/autoload.php'; //so it will automatically load the composers for use when we use "use"
+use Cocur\Slugify\Slugify; // this is the same as import
+
 //  $fullname = 'Railley';
 //  echo "My name is $fullname"; // make sure to use always "" insted of ''
+// -> this symbol is same as . in accessing object keys
+
+  $slugify = new Slugify();
+  echo $slugify->slugify('The Sky is Blue');
 
 // creating function
   function doubleMe($x) {
@@ -14,9 +22,9 @@
   $result =  tripleMe(doubleMe(10));
 
   if($result > 200){
-    echo "the number is 200 higher";
+    echo "\nthe number $result is 200 higher";
   } else {
-    echo "the number is lower than 200";
+    echo "\nthe number $result is lower than 200";
   }
 
  ?>
